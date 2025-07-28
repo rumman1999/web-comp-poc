19350acd412d13bd99bed3173c02b488dd03c204
rumman1999 <you@example.com>
2025-07-28 15:59:24 +0000

    Semantioc Graph component , made the class component

--- src/components/SemanticGraph.ts
+++ src/components/SemanticGraph.ts
@@ -9,4 +9,47 @@ class SemanticGraphs extends HTMLElement {
             mode:'open'
         })
     }
+
+    connectedCallback(){
+        const container = document.createElement('div');
+        container.classList.add('graph-container');
+        this.shadow.appendChild(container);
+        cytoscape({
+            container , 
+            elements:dummyData as ElementDefinition[],
+            layout:{name:'cose' , animate:true},
+            style:[
+        {
+          selector: 'node',
+          style: {
+            'label': 'data(name)',
+            'text-valign': 'center',
+            'background-color': '#61bffc',
+            'shape': 'roundrectangle',
+            'width': 'label',
+            'padding': '10px',
+            'border-color': '#555',
+            'border-width': 1,
+          },
+        },
+        {
+          selector: 'edge',
+          style: {
+            'label': 'data(label)',
+            'width': 2,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier',
+            'font-size': '10px',
+            'text-background-color': '#fff',
+            'text-background-opacity': 1,
+            'text-background-padding': '3px',
+          },
+        },
+      ],
+        })
+    }
 }
+
+customElements.define('semantic-graph' , SemanticGraphs)
